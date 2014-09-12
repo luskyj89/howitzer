483b8364137c915c7c27ce86f93b63f1744a2f38
John Lusky <info@johnlusky.com>
2014-09-12 02:09:15 -0400

    Began adding info layout, animations

--- _uncompressed/js/scripts.js
+++ _uncompressed/js/scripts.js
@@ -1,13 +1,30 @@
 
 function init(){
-  console.log("init");
+    console.log("init");
 
-  $(".main").onepage_scroll({
-    
-  });
+    $(".main").onepage_scroll({
+        loop: false,
+        pagination: false,
+        beforeMove: function(index) {
+            $("#logo").animate({ opacity: .5 },800);
+            $(".entry-info").animate({ opacity: 0 },300);
+        },
+        afterMove: function(index) {
+            $(".entry-info").animate({ opacity: 1 },400);
+        }
+    });
+
+    $("#logo")
+        .fadeIn(1000)
+        .hover(function(){
+        $("#logo").animate({ opacity: 1 },500);
+    }, function(){
+        $("#logo").stop(true, false).animate({ opacity: .5 },500);
+    });
+
 }
 
 
 $(document).ready(function(){
-  init();
+    init();
 });
