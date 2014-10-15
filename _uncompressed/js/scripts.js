1085f40be37527d31b1ad3021f7dc6a2ce6374d5
John Lusky <info@johnlusky.com>
2014-10-15 00:33:58 -0400

    Added photo features, video example

--- _uncompressed/js/scripts.js
+++ _uncompressed/js/scripts.js
@@ -1,4 +1,5 @@
 
+
 function init(){
     console.log("init");
 
@@ -8,6 +9,7 @@ function init(){
         beforeMove: function(index) {
             $("#logo").animate({ opacity: .5 },800);
             $(".entry-info").animate({ opacity: 0 },300);
+            $.fancybox.close( true );
         },
         afterMove: function(index) {
             $(".entry-info").animate({ opacity: 1 },400);
@@ -22,9 +24,30 @@ function init(){
         $("#logo").stop(true, false).animate({ opacity: .5 },500);
     });
 
+
+
 }
 
 
 $(document).ready(function(){
     init();
+
+    $(".video").fitVids();
+
+    $(document).ready(function() {
+	$(".fancybox-thumb").fancybox({
+		prevEffect	: 'fade',
+		nextEffect	: 'fade',
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			thumbs	: {
+				width	: 50,
+				height	: 50
+			}
+		}
+	});
+});
+
 });
